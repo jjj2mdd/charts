7d80e29db49ceb92b52884d4e3eb561056792aae
jjj2mdd <1072586909@qq.com>
2019-05-24 14:59:29 +0800

    Update chart_canvas.dart

--- charts_flutter/lib/src/chart_canvas.dart
+++ charts_flutter/lib/src/chart_canvas.dart
@@ -186,7 +186,7 @@ class ChartCanvas implements common.ChartCanvas {
         if (drawAreaBounds != null) {
           final from = Offset(drawAreaBounds.left.toDouble(), drawAreaBounds.top.toDouble());
           final to = Offset(drawAreaBounds.left.toDouble(), drawAreaBounds.bottom.toDouble());
-          _paint.shader = _createHintGradient(from, to, fill, stroke);
+          _paint.shader = _createGradient(from, to, fill, stroke);
         }
 
         canvas.drawRect(_getRect(fillRectBounds), _paint);
